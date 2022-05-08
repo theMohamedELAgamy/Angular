b7c83ac336b107902f250323a30dcdd089959612
theMohamedELAgamy <98603281+theMohamedELAgamy@users.noreply.github.com>
2022-05-08 12:52:53 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { StudentComponent } from './student/student.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,StudentComponent, HeaderComponent, FooterComponent, SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/department/department.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department',
+  // templateUrl: './department.component.html',
+  template:`<p>hiiiiii</p>`,
+  styleUrls: ['./department.component.css']
+})
+export class DepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
